58bbff3330eb274fd330b97b3c106514a7ae1edb
Leonardo Estevão Castoldi <53437698+leocastoldi@users.noreply.github.com>
2020-06-14 10:14:36 -0300

    Add files via upload

---
+++ EX008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A sua velocidade é de ${vel} KM/h`)
+if (vel > 60) {
+    console.log(`A sua velocidade ${vel} está acima do permitido. MULTADO`)
+}
+console.log(`Dirija sempre com cuidade e use cinto de segurança!`)
